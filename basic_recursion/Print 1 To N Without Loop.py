5ae241d981d3e9739c838a443f1bbd1519b683cd
Annu9111 <aksoni911190@gmail.com>
2025-11-21 06:39:00 +0530

    print 1 to n number

---
+++ basic_recursion/Print 1 To N Without Loop.py
@@ -0,0 +1,8 @@
+#question:https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1
+#code
+class Solution:    
+    def printNos(self,n):
+        if n==0:
+            return 
+        self.printNos(n-1)
+        print(n,end=" ")
