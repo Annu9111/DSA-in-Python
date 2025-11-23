2db8c346f2642064e70018b7cfc53307aac4e7a1
Annu9111 <aksoni911190@gmail.com>
2025-11-23 09:39:44 +0530

    insertion sort

---
+++ sorting/Insertion Sort.py
@@ -0,0 +1,13 @@
+#question:https://www.geeksforgeeks.org/problems/insertion-sort/0
+class Solution:
+    def insertionSort(self, arr):
+        n=len(arr)
+        for i in range(1,n):
+            key=arr[i]
+            j=i-1
+            while j>=0 and arr[j]>key:
+                arr[j+1]=arr[j]
+                j-=1
+            arr[j+1]=key
+        return key
+            
