62144d2d00ed4a515b84f5cc1caa221fafb20c61
Annu9111 <aksoni911190@gmail.com>
2025-11-24 07:49:20 +0530

    maximum consequetive one

---
+++ array/max_consequetive_ones.py
@@ -0,0 +1,16 @@
+#question:https://leetcode.com/problems/max-consecutive-ones/description/
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxi=float("-inf")
+        count=0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                count+=1
+                maxi=max(maxi,count)
+            else:
+                count=0
+        return maxi            
