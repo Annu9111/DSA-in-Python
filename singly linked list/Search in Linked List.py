0d70e7d252c372293419d155afbb41aa0bad414a
Annu9111 <aksoni911190@gmail.com>
2025-11-28 10:34:54 +0530

    search in linked list

---
+++ singly linked list/Search in Linked List.py
@@ -0,0 +1,17 @@
+#question:https://www.geeksforgeeks.org/problems/search-in-linked-list-1664434326/1
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def searchKey(self, head, key):
+        temp=head
+        while temp!=None:
+            if temp.data==key:
+                return True
+            temp=temp.next  
+        return False
+        
