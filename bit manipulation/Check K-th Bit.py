66792ec6b8c73c57310db871d5d19dc6678229a1
Annu9111 <aksoni911190@gmail.com>
2025-11-29 10:38:56 +0530

    check kth bit

---
+++ bit manipulation/Check K-th Bit.py
@@ -0,0 +1,7 @@
+#question:https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1class Solution:
+    def checkKthBit(self, n, k):
+        if n & (1<<k)!=0:
+            return True
+        else:
+            return False
+        
