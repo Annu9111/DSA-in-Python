c693f63927a8170cdea91ac0edf4a5b509f197c4
Annu9111 <aksoni911190@gmail.com>
2025-11-30 11:53:40 +0530

    combination sum 3

---
+++ recursion/Combination Sum III.py
@@ -0,0 +1,26 @@
+#question:https://leetcode.com/problems/combination-sum-iii/
+class Solution(object):
+    def solve(self,index,total,subset,k,n,result,nums):
+        if total==n and len(subset)==k:
+            result.append(subset[::])
+            return
+        if index>=len(nums):
+            return    
+        subset.append(nums[index])
+        self.solve(index+1,total+nums[index],subset,k,n,result,nums)
+        subset.pop()
+        self.solve(index+1,total,subset,k,n,result,nums)        
+
+
+    
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        result=[]
+        nums=[1,2,3,4,5,6,7,8,9]
+        self.solve(0,0,[],k,n,result,nums)
+        return result
+        
