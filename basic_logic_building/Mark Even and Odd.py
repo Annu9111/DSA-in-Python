b28641f06752c36df1a8a546e0c7c4c534b65b81
Annu9111 <aksoni911190@gmail.com>
2025-12-03 15:52:39 +0530

    mark even or odd

---
+++ basic_logic_building/Mark Even and Odd.py
@@ -0,0 +1,9 @@
+#Question:https://www.geeksforgeeks.org/problems/mark-even-and-odd/1?page=1&category=python&sortBy=submissions
+# @param x: int
+# @return: string
+
+def checkOddEven(x):
+    if x%2==0:
+        return "Even"
+    else:
+        return "Odd"
