5e0c036c0ffd7088068c5f63bb812476fb17c0b3
Annu9111 <aksoni911190@gmail.com>
2025-12-03 07:25:21 +0530

    max consecutive ones

---
+++ sliding window/Max Consecutive Ones III.py
@@ -0,0 +1,25 @@
+#question:https://leetcode.com/problems/max-consecutive-ones-iii/description/
+class Solution(object):
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        maxi=0
+        n=len(nums)
+        left=0
+        right=0
+        zero=0
+        while right<n:
+            if nums[right]==0:
+                zero+=1
+            if zero>k:
+                if nums[left]==0:
+                    zero-=1
+                left+=1
+            if zero<=k:
+                maxi=max(maxi,right-left+1)
+            right+=1
+        return maxi                    
+        
