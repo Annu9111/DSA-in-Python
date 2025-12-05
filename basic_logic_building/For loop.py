d4c9f39f9d77e1c4cd332ab3fad3508891a82144
Annu9111 <aksoni911190@gmail.com>
2025-12-05 06:44:26 +0530

    for loop

---
+++ basic_logic_building/For loop.py
@@ -0,0 +1,4 @@
+#question:https://www.geeksforgeeks.org/problems/for-loop-python/1?page=1&category=python&difficulty=Easy&sortBy=submissions
+def multiplicationTable(N):
+    for i in range(1,11):
+        print(N*i ,end=" ")
