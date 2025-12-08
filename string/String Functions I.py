a9f7953e02beab07bc09d56af7f6322107ecc26f
Annu9111 <aksoni911190@gmail.com>
2025-12-08 07:04:03 +0530

    string function

---
+++ string/String Functions I.py
@@ -0,0 +1,15 @@
+#question:https://www.geeksforgeeks.org/problems/string-functions-i/1?page=1&category=python&sortBy=submissions
+def trim(str):
+    return str.strip()
+
+
+def exists(str, x):
+    return str.find(x)
+
+
+def titleIt(str):
+    return str.title()
+
+
+def casesSwap(str):
+    return str.swapcase()
