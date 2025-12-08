5fa77822f2beeedd6e38292d1114ca28983a9006
Annu9111 <aksoni911190@gmail.com>
2025-12-08 06:55:34 +0530

    welcome abroad

---
+++ basic_logic_building/Welcome aboard.py
@@ -0,0 +1,4 @@
+#Question:https://www.geeksforgeeks.org/problems/welcome-aboard-python/1?page=1&category=python&sortBy=submissions
+# Function to Welcome the person
+def welcomeAboard(name):
+    print("Welcome",name)  # Your code here
