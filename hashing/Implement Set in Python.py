ac7a287d1d2332a74a8960a5ec419c71210abf21
Annu9111 <aksoni911190@gmail.com>
2025-12-10 07:07:06 +0530

    implement set

---
+++ hashing/Implement Set in Python.py
@@ -0,0 +1,29 @@
+#Question:https://www.geeksforgeeks.org/problems/implement-set-in-python--141631/1?page=2&category=python&sortBy=submissions
+#User function Template for python3
+
+# Function to insert
+# no return should be there, and no print statement
+def insert(s, element):
+    # Your code here
+    s.add(element)
+    
+    
+    
+    
+
+# Function to remove element from set
+# No return and nothing to print
+def remove_from_set(s, element):
+    # Your code here
+    s.remove(element)
+    
+    
+    
+    
+# Function to find sum of elements in set
+# return value should be there as sum
+def sum_set(s):
+    # Your code here
+    return sum(s)
+    
+    
