f1293c692cedced71c13a2e2b936b33804cfb5b5
Annu9111 <aksoni911190@gmail.com>
2025-12-11 14:48:01 +0530

    reverse a string

---
+++ basic_logic_building/Reverse a String.py
@@ -0,0 +1,6 @@
+#question:https://www.geeksforgeeks.org/problems/reverse-a-string/1?page=1&category=Strings,python&sortBy=submissions
+#User function Template for python3
+
+class Solution:
+     def reverseString(self, s: str) -> str:
+        return s[::-1]
