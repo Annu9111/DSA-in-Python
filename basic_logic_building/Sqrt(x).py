c5f235d6597e6490fc1756688657a73d24eb7044
Annu9111 <aksoni911190@gmail.com>
2025-12-11 13:01:40 +0530

    sqrt of

---
+++ basic_logic_building/Sqrt(x).py
@@ -0,0 +1,10 @@
+#question:https://leetcode.com/problems/sqrtx/description/?envType=problem-list-v2&envId=math
+from math import sqrt
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(sqrt(x))
+        
