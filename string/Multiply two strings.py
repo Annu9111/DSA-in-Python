d145736185ad1092cde73d7955777ab15079348d
Annu9111 <aksoni911190@gmail.com>
2025-12-13 11:43:46 +0530

    string product

---
+++ string/Multiply two strings.py
@@ -0,0 +1,7 @@
+#question:https://www.geeksforgeeks.org/problems/multiply-two-strings/1?page=1&category=Strings,python&sortBy=submissions
+class Solution:
+    def multiplyStrings(self, s1, s2):
+        a=int(s1)
+        b=int(s2)
+        return a*b
+        
