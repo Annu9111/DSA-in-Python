6217304562cc1cfab8d0a36664b57c6043decca1
Annu9111 <aksoni911190@gmail.com>
2025-12-18 07:20:13 +0530

    merge string

---
+++ string/Merge Strings Alternately.py
@@ -0,0 +1,25 @@
+# question:https://leetcode.com/problems/merge-strings-alternately/description/?envType=problem-list-v2&envId=two-pointers
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result=""
+        i,j=0,0
+        n,m=len(word1),len(word2)
+        while i<n and j<m:
+            result+=word1[i]
+            result+=word2[j]
+            i+=1
+            j+=1
+
+        while i<n:
+            result+=word1[i]
+            i+=1
+        while j<m:
+            result+=word2[j]
+            j+=1
+        return result            
+        
