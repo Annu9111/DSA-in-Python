c6b1887a795321db678d6fc135883b3d64a3b6a1
Annu9111 <aksoni911190@gmail.com>
2025-12-21 17:11:34 +0530

    zero converter

---
+++ string/Zero Converter - Python.py
@@ -0,0 +1,13 @@
+# https://www.geeksforgeeks.org/problems/zero-converter-python/1?page=1&category=Sorting,python&difficulty=Basic&sortBy=submissions
+def pos(n):
+    ## Write the code
+    for i in range(n-1,-1,-1):
+        print(i,end=" ") 
+        
+        
+    
+def neg(n):
+    ##Write the code
+    for i in range(n,1):
+        print(i,end=" ")
+    
