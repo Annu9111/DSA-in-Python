c66b99ff7689b2792faee7c7a614b80fdea19b0f
Annu9111 <aksoni911190@gmail.com>
2025-12-23 13:11:28 +0530

    jay's apples

---
+++ hashing/Jay's Apples.py
@@ -0,0 +1,7 @@
+# https://www.geeksforgeeks.org/problems/jays-apples2724/1?page=1&category=python,set&difficulty=Basic&sortBy=submissions
+#User function Template for python3
+
+class Solution:
+    def minimumApple(self, arr):
+        # Complete the function
+        return len(set(arr))
