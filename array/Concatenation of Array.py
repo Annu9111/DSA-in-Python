12ec4d82b2ffbc9938d62d9fc1b1bfbe249fcffb
Annu9111 <aksoni911190@gmail.com>
2025-12-24 13:10:03 +0530

    concatenate array

---
+++ array/Concatenation of Array.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/concatenation-of-array/description/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=nums+nums
+        return ans
+
+    
+        
