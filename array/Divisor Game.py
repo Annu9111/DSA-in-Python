adc989557708c028b12eeb4009c2d03523e28923
Annu9111 <aksoni911190@gmail.com>
2025-12-28 11:42:38 +0530

    divisor game

---
+++ array/Divisor Game.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/divisor-game/description/
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n%2==0        
+        
